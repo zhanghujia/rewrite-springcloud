27ae244b801ee0ca68d066b112c8231891f66364
zhanghujia <18305178922@163.com>
2020-12-21 11:47:00 +0800

    2020/12/21-3

--- spring-cloud-config-server/src/main/java/com/example/config/ConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/example/config/ConfigServerApplication.java
@@ -3,15 +3,18 @@ package com.example.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author 10696
  * @since 2020/12/14 17:29
  */
+
+@EnableConfigServer
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class,args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
